52714819e67211a1c664bb095353cab792dd0bc3
Sebastian Stark <sstark@mailbox.org>
2018-09-21 19:33:27 +0200

    accept a route map in the server, so api can be used from RenderContext

--- render.py
+++ render.py
@@ -5,14 +5,23 @@ from threading import Thread
 
 class BaseRenderContext():
     def __init__(self, drivername, conf):
+        self.conf = conf
         self.width = conf["resolution"]["x"]
         self.height = conf["resolution"]["y"]
         self.fps = conf["maxfps"]
         self.image = Image.new('1', (self.width, self.height), 255)
         self.startWebserver()
 
+    def apiGetAreas(self):
+        return [ x["name"] for x in self.conf["areas"] ]
+
+    def makeRouteMap(self):
+        return {
+            "getAreas": self.apiGetAreas
+        }
+
     def startWebserver(self):
-        self.webserver = webserver.WebServer()
+        self.webserver = webserver.WebServer(self.makeRouteMap())
         self.server = Thread(target=self.webserver.run)
         self.server.daemon = True
         self.server.start()
--- webserver.py
+++ webserver.py
@@ -1,23 +1,48 @@
 
-#import socket
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from functools import partial
+import json
 
 class WebAPI(BaseHTTPRequestHandler):
-    
+
+    def __init__(self, routeMap, *args, **kwargs):
+        self.apiFuncs = routeMap
+        self.urlsGET = {
+            "/v1/areas": self.getApiFunc("getAreas")
+        }
+        super().__init__(*args, **kwargs)
+
+    def unknownApiFunc(self):
+        print("unknown API function requested")
+
+    def getApiFunc(self, name):
+        return self.apiFuncs.get(name, self.unknownApiFunc)
+
+    def matchUrl(self, routes):
+        return routes.get(self.path, None)
+
     def do_GET(self):
-        self.send_response(200)
-        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
+        func = self.matchUrl(self.urlsGET)
+        if func:
+            out = func()
+            self.send_response(200)
+        else:
+            out = "not found"
+            self.send_response(404)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(bytes(json.dumps(out).encode("utf-8")))
 
     def do_PUT(self):
-        print("PUT", self.path)
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         self.send_response(200)
         client.close()
 
 class WebServer():
-    def __init__(self, hostname="localhost", port=2354):
-        self.server = HTTPServer((hostname, port), WebAPI)
+    def __init__(self, routeMap, hostname="localhost", port=2354):
+        requestHandler = partial(WebAPI, routeMap)
+        self.server = HTTPServer((hostname, port), requestHandler)
         print("starting webserver (%s:%s)" % (hostname, port))
 
     def run(self):
