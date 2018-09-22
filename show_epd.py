0b9f2db9c9bb086a0fd2696507f2dd7754f8a8f3
Sebastian Stark <sstark@mailbox.org>
2018-09-22 03:02:03 +0200

    very basic but working version of epd RenderContext
      - does not make use yet of the partial update features
      - in need of event handling, as we should not update
      the epaper at full fps all the time
      - epd2in9 needed some minor modifications to work with
      python3
      - epd2in9 dos line endings are kept so it is easier to diff
      when there is a vendor update

--- show_epd.py
+++ show_epd.py
@@ -2,9 +2,10 @@
 import importlib
 import sys
 from render import BaseRenderContext
+import time
 
 class RenderContext(BaseRenderContext):
-    def __init__(self, drivername, conf):
+    def __init__(self, drivername, conf, scale=1):
         try:
             self.driver = importlib.import_module("epd."+drivername)
         except ModuleNotFoundError as e:
@@ -12,7 +13,28 @@ class RenderContext(BaseRenderContext):
             print(e)
             sys.exit(1)
         print("init epd output module (driver=%s)" % drivername)
-        super().__init__(drivername, conf)
+        super().__init__(drivername, conf, scale)
+        self.delay = 1000//self.fps
+        self.epd = self.driver.EPD()
+        self.epd.init(self.epd.lut_partial_update)
+        self.clearPaper()
+
+    def clearPaper(self):
+        self.epd.clear_frame_memory(0x00)
+        self.epd.display_frame()
+        self.epd.clear_frame_memory(0xFF)
+        self.epd.display_frame()
+
+    def display(self):
+        self.epd.set_frame_memory(self.image.rotate(270, expand=1), 0, 0)
+        self.epd.display_frame()
 
     def run(self):
         print("run epd output module")
+        try:
+            while True:
+                self.display()
+                time.sleep(2)
+        except KeyboardInterrupt:
+            pass
+
